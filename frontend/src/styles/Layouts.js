a2d9f77ce2a72e13b87974325a77378f946fc4e1
namneet08 <129228229+namneet08@users.noreply.github.com>
2023-11-26 09:20:56 -0500

    fixed error in controllers and made frontend file structure

---
+++ frontend/src/styles/Layouts.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const MainLayout = styled.div`
+    padding: 1rem;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    gap: 0.5rem;
+    align-items:center;
+    margin:auto;
+    
+`;
+
+export const InnerLayout = styled.div`
+    padding: 0.5rem 0.5rem;
+    width: 100%;
+`;

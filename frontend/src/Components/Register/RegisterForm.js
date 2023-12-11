5e42beb16f464e3fd99bb481594a4aec335f6a6d
m4ejs <145816886+m4ejs@users.noreply.github.com>
2023-12-11 06:17:50 +0500

    Fixed the RegisterForm.js file

--- frontend/src/Components/Register/RegisterForm.js
+++ frontend/src/Components/Register/RegisterForm.js
@@ -63,37 +63,7 @@ function Form() {
                     name={'Add User'}
                     icon={plus}
                     bPad={'.8rem 1.6rem'}
-                    bRad={'30px'}
-                    bg={'var(--color-accent'}
-                    color={'#fff'}
-                />
-            </div>
-        </FormStyled>
-    )
-}
-
-
-const FormStyled = styled.form`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    input, textarea, select{
-        font-family: inherit;
-        font-size: inherit;
-        outline: none;
-        border: none;
-        padding: .5rem 1rem;
-        border-radius: 5px;
-        border: 2px solid #fff;
-        background: transparent;
-        resize: none;
-        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
-        color: rgba(34, 34, 96, 0.9);
-        &::placeholder{
-            color: rgba(34, 34, 96, 0.4);
-        }
-    }
-    .input-control{
+       ntrol{
         input{
             width: 100%;
         }

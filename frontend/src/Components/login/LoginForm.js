5b88d4ac9f0ed739386c13142e011655580265bf
m4ejs <145816886+m4ejs@users.noreply.github.com>
2023-12-11 06:16:50 +0500

    Update and fixed the LoginForm.js file

--- frontend/src/Components/login/LoginForm.js
+++ frontend/src/Components/login/LoginForm.js
@@ -28,63 +28,6 @@ function Form() {
         })
     }
 
-    return (
-        <FormStyled onSubmit={handleSubmit}>
-            {error && <p className='error'>{error}</p>}
-            <div className="input-control">
-                <input value={email}  
-                    type="email" 
-                    name={'email'} 
-                    placeholder={'Enter your email address'}
-                    onChange={handleInput('email')} 
-                />
-            </div>
-            <div className="input-control">
-                <input value={password}  
-                    type="password" 
-                    name={'password'} 
-                    placeholder={'Enter password'}
-                    onChange={handleInput('password')} 
-                />
-            </div>
-            <div className="submit-btn">
-                <Button 
-                    name={'Add User'}
-                    icon={plus}
-                    bPad={'.8rem 1.6rem'}
-                    bRad={'30px'}
-                    bg={'var(--color-accent'}
-                    color={'#fff'}
-                />
-            </div>
-        </FormStyled>
-    )
-}
-
-
-const FormStyled = styled.form`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    input, textarea, select{
-        font-family: inherit;
-        font-size: inherit;
-        outline: none;
-        border: none;
-        padding: .5rem 1rem;
-        border-radius: 5px;
-        border: 2px solid #fff;
-        background: transparent;
-        resize: none;
-        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
-        color: rgba(34, 34, 96, 0.9);
-        &::placeholder{
-            color: rgba(34, 34, 96, 0.4);
-        }
-    }
-    .input-control{
-        input{
-            width: 100%;
         }
     }
 

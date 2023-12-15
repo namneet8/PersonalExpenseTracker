05d662cb9eb2995197e010cdcf895236b2be1deb
m4ejs <145816886+m4ejs@users.noreply.github.com>
2023-12-15 05:10:54 +0500

    Fixed ExpenseForm.js

--- frontend/src/Components/Expenses/ExpenseForm.js
+++ frontend/src/Components/Expenses/ExpenseForm.js
@@ -7,73 +7,7 @@ import Button from '../Button/Button';
 import { plus } from '../../utils/icons';
 
 
-function ExpenseForm() {
-    const {addExpense, error, setError} = useGlobalContext()
-    const [inputState, setInputState] = useState({
-        title: '',
-        amount: '',
-        date: '',
-        category: '',
-        description: '',
-    })
-
-    const { title, amount, date, category,description } = inputState;
-
-    const handleInput = name => e => {
-        setInputState({...inputState, [name]: e.target.value})
-        setError('')
-    }
-
-    const handleSubmit = e => {
-        e.preventDefault()
-        addExpense(inputState)
-        setInputState({
-            title: '',
-            amount: '',
-            date: '',
-            category: '',
-            description: '',
-        })
-    }
-
-    return (
-        <ExpenseFormStyled onSubmit={handleSubmit}>
-            {error && <p className='error'>{error}</p>}
-            <div className="input-control">
-                <input 
-                    type="text" 
-                    value={title}
-                    name={'title'} 
-                    placeholder="Expense Title"
-                    onChange={handleInput('title')}
-                />
-            </div>
-            <div className="input-control">
-                <input value={amount}  
-                    type="text" 
-                    name={'amount'} 
-                    placeholder={'Expense Amount'}
-                    onChange={handleInput('amount')} 
-                />
-            </div>
-            <div className="input-control">
-                <DatePicker 
-                    id='date'
-                    placeholderText='Enter A Date'
-                    selected={date}
-                    dateFormat="dd/MM/yyyy"
-                    onChange={(date) => {
-                        setInputState({...inputState, date: date})
-                    }}
-                />
-            </div>
-            <div className="selects input-control">
-                <select required value={category} name="category" id="category" onChange={handleInput('category')}>
-                    <option value="" disabled >Select Option</option>
-                    <option value="education">Education</option>
-                    <option value="groceries">Groceries</option>
-                    <option value="health">Health</option>
-                    <option value="subscriptions">Subscriptions</option>
+f
                     <option value="takeaways">Takeaways</option>
                     <option value="clothing">Clothing</option>  
                     <option value="travelling">Travelling</option>  

2f04913e46325bcd7c1ef2d76a47225801c0afcd
m4ejs <145816886+m4ejs@users.noreply.github.com>
2023-12-15 05:11:32 +0500

    Fixed Expenses.js

--- frontend/src/Components/Expenses/Expenses.js
+++ frontend/src/Components/Expenses/Expenses.js
@@ -5,60 +5,7 @@ import { useGlobalContext } from '../context/globalContext';
 import IncomeItem from '../IncomeItem/IncomeItem';
 import ExpenseForm from './ExpenseForm';
 
-function Expenses() {
-    const {addIncome,expenses, getExpenses, deleteExpense, totalExpenses} = useGlobalContext()
 
-    useEffect(() =>{
-        getExpenses()
-    }, [])
-    return (
-        <ExpenseStyled>
-           <div className='form'>
-            <div className='container'>
-                <h1>Expenses</h1>
-                <h2 className="total-income">Total Expense: <span>${totalExpenses()}</span></h2>
-                <div className="income-content">
-                    <div className="form-container">
-                        <ExpenseForm />
-                    </div>
-                    <div className="incomes">
-                        {expenses.map((income) => {
-                            const {_id, title, amount, date, category, description, type} = income;
-                            console.log(income)
-                            return <IncomeItem
-                                key={_id}
-                                id={_id} 
-                                title={title} 
-                                description={description} 
-                                amount={amount} 
-                                date={date} 
-                                type={type}
-                                category={category} 
-                                indicatorColor="green"
-                                deleteItem={deleteExpense}
-                            />
-                        })}
-                    </div>
-                </div>
-                </div>
-                </div>
-            
-        </ExpenseStyled>
-    )
-}
-
-const ExpenseStyled = styled.div`
-.form{
-    padding:60px;
-    
-}
-.container{
-    background-color: salmon;
-    padding:20px;
-}
-
-    display: flex;
-    flex-direction:column;
     overflow: auto;
     .total-income{
         display: flex;
